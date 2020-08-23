3769663814b01217b19a8d852f52c27d803b4e2b
szwxj <13501586491@139.com>
2020-08-23 21:56:41 +0800

    week02 作业 242题，学习python字典语法，写的较为复杂

--- Week_02/242.py
+++ Week_02/242.py
@@ -1,19 +1,5 @@
-'''
 import collections
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return collections.Counter(s) == collections.Counter(t)
-
-
-s = 'a'
-t = 'b'
-
-ss = Solution()
-
-print(ss.isAnagram(s,t))
-'''
-'''
 class MyPoints:
     def __init__(self,x,y):
         self.x = x
@@ -30,55 +16,49 @@ class MyPoints:
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        myDict = {}
-        #po = MyPoints(0,0)
-        #for i in 'abcdefghijklmnopqrstuvwxyz' :
-        #    myDict[i] = po
+        return self.isAnagram3(s,t)
 
-        i = 0
-        while i<len(s):
-            if any(myDict) and any(myDict[s[i]]):
-                myDict[s[i]].addX
-                print(s[i],myDict[s[i]].x)
-            else:
-                myDict[s[i]] = MyPoints(1,0)
-
-            #po.addX
-            i+=1
-
-        i = 0 
-        while i<len(t):
-            if any(myDict[t[i]]):
-                myDict[t[i]].addY
-                print(t[i],myDict[t[i]].y)
-            else:
-                myDict[t[i]] = MyPoints(0,1)
-            #po.addY
-            i+=1
-
-        for key,value in myDict.items():
-            #po = values
-            print(key,value.x,value.y)
-            if not value.isEqual :
-                return False 
-        return True 
-
-'''
-class Solution(object):
-    def isAnagram(self, s, t):
+    def isAnagram1(self, s: str, t: str) -> bool:
+        return collections.Counter(s) == collections.Counter(t)
+    
+    def isAnagram2(self, s: str, t: str) -> bool:
     	if sorted(s) == sorted(t):
     		return True
     	else:
     		return False
 
+
+    def isAnagram3(self, s: str, t: str) -> bool:
+        myDict = {}
+        #po = MyPoints(0,0)
+        #for i in 'abcdefghijklmnopqrstuvwxyz' :
+        #    myDict[i] = MyPoints(0,0)
+
+        i = 0
+        while i<len(s):
+            if any(myDict) and (myDict.get(s[i],None) != None):
+                myDict[s[i]].addX()
+                print(s[i],myDict[s[i]].x)
+            else:
+                myDict[s[i]] = MyPoints(1,0)
+            i+=1
+
+        i = 0 
+        while i<len(t):
+            if any(myDict) and (myDict.get(t[i],None) != None):
+                myDict[t[i]].addY()
+                print(t[i],myDict[t[i]].y)
+            else:
+                myDict[t[i]] = MyPoints(0,1)
+            i+=1
+
+        for key,value in myDict.items():
+            #print(key,value.x,value.y)
+            if not value.isEqual() :
+                return False 
+        return True 
+
+
 s = Solution()
-print(s.isAnagram('a','b'))
-
-myDict = {}
-
-myDict['a'] = [0,0]
-if 'b' in myDict.keys():
-    print(True)
-else:
-    print(False)
+print(s.isAnagram('abbc','baac'))
 
